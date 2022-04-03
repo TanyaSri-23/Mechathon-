daf13163437c9a0634aa1e8ca17e5c3b4a79a364
vardanMittal <vardanmittal000@gmail.com>
2022-04-03 11:26:29 +0530

    updated code for correct force analysis

--- structural_analysis.py
+++ structural_analysis.py
@@ -1,20 +1,15 @@
 from math import *
 import pandas as pd
 forces = []
-angle = []
-coordinates = []
 """This is a command line program of structural analysis"""
 
-def force_analysis(mag, orientation):
-    H = 0
-    V = 0
-    print(mag, orientation)
-    for F, a in mag, orientation:
-        V += F*sin(a)
-        H += F*cos(a)
-        print(V,H)
-    
-    return (round(H,2), round(V,2))
+def force_analysis(F):
+    H = []
+    V = []
+    for force in F:
+        V.append(round(force[0]*sin(force[2]),2))
+        H.append(round(force[0]*cos(force[2]),2))
+    return (round(sum(H),2), round(sum(V),2))
 
 def torque_analysis(mag, pos):
     pass
@@ -23,15 +18,19 @@ def torque_analysis(mag, pos):
 nf = int(input("Enter number of forces: "))
 
 for i in range(0,nf):
+    force=[]
     F = int(input(f"Enter the magnitude of F{i+1}: "))
-    forces.append(F)
+    force.append(F)
     position = tuple(input("Enter the cartesian coordinates of point of action: (x,y)").split(","))
-    coordinates.append(position)
-    orientation = (float(input("Enter the angle with horizontal(counter clockwise): ")) * (pi/180))
-    angle.append(orientation)
+    force.append(position)
+    orientation = round((float(input("Enter the angle with horizontal(counter clockwise): "))*pi) / 180, 2)
+    force.append(orientation)
+    forces.append(force)
 
 
-data = pd.DataFrame([forces,angle,coordinates], ["magnitude", "angle", "coordinates"])
+for i in forces:
+    data = pd.DataFrame([i[0],i[1],i[2]], ["magnitude", "coordinates", "orientation"])
+
 print(data)
-print(force_analysis(forces, angle))
+print(force_analysis(forces))
 
